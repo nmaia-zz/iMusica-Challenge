2de2d717b19cfb2a754ef172c2ed9bf16017fefa
Natanael Maia <gm.natanael@gmail.com>
2017-03-15 01:39:30 -0300

    Client
    Starting the implementation of the front-end side.

---
+++ iMusica-Client/Project.FrontEnd/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Project.FrontEnd.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // GET: Employee/GetAll
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+
+        // GET: Employee/Edit
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
